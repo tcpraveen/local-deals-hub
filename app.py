6d6e75e00fdf9dbfdb815b7bf7d06008ace5f10e
tcpraveen <tcpraveen123@gmail.com>
2026-07-10 00:19:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,9 @@ if not SUPABASE_URL or not SUPABASE_KEY:
         SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
     except Exception:
         st.error("Missing Supabase API keys in Render environment secrets.")
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # 4. Sidebar - Shopkeeper Portal
 with st.sidebar:
